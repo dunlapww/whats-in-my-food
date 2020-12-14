6cb48e20fc8cc0190ec70b607079cdbe58ac4002
Will Dunlap <dunlapww@gmail.com>
2020-12-14 10:20:47 -0700

    Complete feature spec for display

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -10,7 +10,17 @@ describe 'As a User' do
     it "I'm directed to /foods and the number of foods" do
       expect(current_path).to eq('/foods')
     end
-    it "and I see a list of 10 foods that contain the ingredient 'sweet potatoes'"
-    it 'and for each food I see the fods GTIN/UPC code, te foods description, brand owner, and ingredients'
+    it "and I see a list of 10 foods that contain the ingredient 'sweet potatoes'" do
+      expect(page).to have_content("Below are the first 10 results")
+      expect(page).to have_css(".food", count: 10)
+    end
+    it 'and for each food I see the fods GTIN/UPC code, the foods description, brand owner, and ingredients' do
+      within(first(".food")) do
+        expect(page).to have_css('.description')
+        expect(page).to have_css('.gtiupc')
+        expect(page).to have_css('.brand_owner')
+        expect(page).to have_css('.ingredients')
+      end
+    end
   end
 end
